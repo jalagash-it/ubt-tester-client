481394146b35bf617938b9360a611d9b0d8152bb
yersultan.saduakhas <yersultan.saduakhas@asia-soft.kz>
2021-06-21 18:53:29 +0600

    bug fix

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,32 +1,66 @@
 <template>
-  <v-container>
-    <!-- <v-navigation-drawer>
-            <v-divider></v-divider>
-            <v-list nav dense>
-                <v-list-item @click="changeCurrentComponent('userProfiles')">
-                    <v-list-item-icon>
-                        <v-icon color="white" small>mdi-lightbulb</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-title class="title">Profile</v-list-item-title>
-                </v-list-item>
-                <v-list-item @click="changeCurrentComponent('adminLessons')">
-                    <v-list-item-icon>
-                        <v-icon color="white" small>mdi-lightbulb</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-title class="title">Lessons</v-list-item-title>
-                </v-list-item>
-                <v-list-item @click="changeCurrentComponent('adminUsers')">
-                    <v-list-item-icon>
-                        <v-icon color="white" small>mdi-lightbulb</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-title class="title">Users</v-list-item-title>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer> -->
-        <!-- <v-main>
-            <component :is="child"/>
-        </v-main> -->
-    <v-card>
+  <v-container fluid fill-height>
+      <v-card class="mx-auto overflow-hidden">
+        <v-system-bar color="deep-purple darken-3"></v-system-bar>
+
+        <v-app-bar color="deep-purple accent-4" dark prominent>
+          <v-app-bar-nav-icon
+            @click.stop="drawer = !drawer"
+          ></v-app-bar-nav-icon>
+
+          <v-toolbar-title>{{pages[currentPageIndex].title}}</v-toolbar-title>
+
+          <v-spacer></v-spacer>
+
+          <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-filter</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-app-bar>
+
+        <v-navigation-drawer v-model="drawer" absolute bottom temporary>
+          <v-list nav dense>
+            <v-list-item-group
+              v-model="group"
+              active-class="deep-purple--text text--accent-4"
+            >
+              <v-list-item>
+                <v-list-item-title @click="setCurrentPage(0)">Profile</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-title @click="setCurrentPage(1)">Users</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-title @click="setCurrentPage(2)">Lessons</v-list-item-title>
+              </v-list-item>
+             
+            </v-list-item-group>
+          </v-list>
+        </v-navigation-drawer>
+        <v-main fluid fill-height>
+          <userProfiles v-if="currentPageIndex==0"></userProfiles>
+          <adminUsers v-if="currentPageIndex==1"></adminUsers>
+          <adminLessons v-if="currentPageIndex==2"></adminLessons>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+        </v-main>
+      </v-card>
+    </v-container>
+</template>
+    <!--v-card>
       <v-toolbar>
         <v-toolbar-title>Dashboard</v-toolbar-title>
       </v-toolbar>
@@ -53,28 +87,45 @@
           <adminUsers></adminUsers>
         </v-tab-item>
       </v-tabs>
-    </v-card>
+    </v-card
   </v-container>
-</template>
+</template>-->
 <script>
-import userProfiles from '~/pages/user/profile/index.vue'
-import adminLessons from '~/pages/user/admin/lessons/index.vue'
-import adminUsers from '~/pages/user/admin/users/index.vue'
+import userProfiles from "~/pages/user/profile/index.vue";
+import adminLessons from "~/pages/user/admin/lessons/index.vue";
+import adminUsers from "~/pages/user/admin/users/index.vue";
 export default {
-    data(){
-        return{
-            child:'userProfiles'
-        }
+  data() {
+    return {
+      child: "userProfiles",
+       drawer: false,
+      group: null,
+      currentPageIndex:0,
+
+      pages:[
+        {title:"Profile",key:"profile"},
+        {title:"Users",key:"users"},
+        {title:"lessons",key:"lessons"},
+      ]
+    };
+  },
+  methods: {
+    changeCurrentComponent(key_) {
+      this.child = key_;
     },
-    methods:{
-        changeCurrentComponent(key_){
-            this.child = key_;
-        }
+    setCurrentPage(ind){
+      this.currentPageIndex = ind;
+    } 
+  },
+  components: {
+    userProfiles,
+    adminLessons,
+    adminUsers,
+  },
+  watch: {
+      group () {
+        this.drawer = false
+      },
     },
-    components: {
-        userProfiles,
-        adminLessons,
-        adminUsers
-    }
-}
+};
 </script>
